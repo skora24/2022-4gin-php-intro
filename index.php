5e3f3d07482cf320e44021b24cc7ad5b8a56cb5e
skora24 <bartek.skora23@interia.pl>
2022-09-26 13:18:33 +0200

    zad3

--- index.php
+++ index.php
@@ -1,22 +1,57 @@
 <?php
-$ipsum="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porttitor sed metus vitae porttitor. Suspendisse massa nulla, consequat et pulvinar tincidunt, dapibus in odio. Nunc id pretium nunc. Aliquam vulputate congue felis. In semper cursus ligula, vitae semper nulla aliquet in. Quisque ut mi bibendum quam congue dictum non ac dolor. Cras in purus id felis tempor interdum ut a nulla. Vivamus nec eleifend nulla. Nulla ultricies mi quis tellus blandit semper. Maecenas lacinia ac neque quis congue. Mauris ultrices quam ullamcorper scelerisque consequat. Nullam cursus quam sed tortor volutpat hendrerit. Donec eleifend tincidunt sapien, non vulputate tellus venenatis pretium. ";
-$array = explode(' ', $ipsum);
-var_dump($array);
+$ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium turpis vel nisi dapibus, eget gravida urna maximus. Vivamus faucibus accumsan orci. Ut pulvinar aliquam lorem quis suscipit. Nunc est velit, condimentum sed nisi ac, egestas rhoncus nulla. Cras nec nisi nec neque ultrices vestibulum. Morbi eu libero gravida, aliquet sapien id, mollis nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin risus metus, feugiat quis felis quis, laoreet imperdiet turpis. Proin vulputate ultricies sem, vel tincidunt libero porttitor at. Nam eu mattis sapien, ac malesuada nisi. Aliquam condimentum, velit semper auctor interdum, enim.";
 
 
+$wyszukiwanie = "b";
 
 
-$wyszukiwanie="";
-sort($array, SORT_NATURAL | SORT_FLAG_CASE);
+function sortandsearch($text, $look){
+   
+    
+$S = explode(" ", $text);
 
-foreach ($array as $w ){
-    if(preg_match("/\b(\w*$wyszukiwanie\w*)\b/", $w, $match) == true){
-        echo " $match[0] </br>";
+sort($S, SORT_NATURAL | SORT_FLAG_CASE);
+
+foreach($S as $slowo){
+    if(preg_match("/\b(\w*$look\w*)\b/", $slowo, $match) == true){
+    echo "$match[0] </br>";
     }
-
 }
 
+};
 
+sortandsearch($ipsum, $wyszukiwanie);
 
-
-?>
+function renderHTMLTable($ilosc, $tak){ 
+    $S = explode(" ", $tak);
+$liczba = 1;
+echo '<table>';
+foreach ($S as $wyraz){
+    if ($liczba <= $ilosc){
+        if($liczba % $ilosc == 1){
+            echo "<tr><th>".$wyraz."</th>";
+        }
+        else if ($liczba % $ilosc == 0){
+            echo "<th>".$wyraz."</th></tr>";
+        }
+        else {
+            echo "<th>".$wyraz."</th>";
+        }
+     } else {
+            if($liczba % $ilosc == 1){
+                echo "<tr><td>".$wyraz."</td>";
+            }
+            else if ($liczba % $ilosc == 0){
+                echo "<td>".$wyraz."</td></tr>";
+            }
+            else {
+                echo "<td>".$wyraz."</td>";
+            }
+        }
+        $liczba++;
+    }
+    echo "</table>";
+}
+$kolumna = "4";
+renderHTMLTable ($kolumna, $ipsum);
+?> 
